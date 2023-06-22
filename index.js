2777cfd785a40afbd44a81582159d17b8460ebf9
Asriths31 <asrithsai840@gmail.com>
2023-06-22 20:10:19 +0530

    updated twice

--- index.js
+++
@@ -1,9 +0,0 @@
-let a=document.getElementsByClassName("press")
-document.querySelector(".press").addEventListener("click",function(){
-    document.querySelector(".press").innerHTML=
-    `<ul>
-            <li><a href="#rope">whythis</a></li>
-            <li><a href="#trans">components</a></li>
-            <li><a href="#scam">working</a></li>
-     </ul>                                           `
-})
