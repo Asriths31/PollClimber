c2078b2d7b52642c519cf299f54d4d8037cc6d8a
Asriths31 <asrithsai840@gmail.com>
2023-06-22 20:06:54 +0530

    added menubar

---
+++ index.js
@@ -0,0 +1,9 @@
+let a=document.getElementsByClassName("press")
+document.querySelector(".press").addEventListener("click",function(){
+    document.querySelector(".press").innerHTML=
+    `<ul>
+            <li><a href="#rope">whythis</a></li>
+            <li><a href="#trans">components</a></li>
+            <li><a href="#scam">working</a></li>
+     </ul>                                           `
+})
