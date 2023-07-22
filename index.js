95d7a0809ffd01effbf03998a6f8924ac1512636
Asriths31 <asrithsai840@gmail.com>
2023-07-22 19:55:13 +0530

    ....

--- index.js
+++
@@ -1,9 +0,0 @@
-function response(){
-    var x=document.getElementById("opt");
-    if(x.className==="yu"){
-        x.classname+="responsive";
-    }
-    else{
-        x.classname="yu";
-    }
-}
